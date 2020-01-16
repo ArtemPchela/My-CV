d1e91e2642ec18460aadec8ce2feb59d86512080
“Name <pchelenkovartem@gmail.com>
2020-01-16 16:09:37 +0100

    add new content

--- src/components/education.js
+++ src/components/education.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { Grid, Cell } from "react-mdl/lib/Grid";
+import React, {Component} from 'react';
+import {Grid, Cell} from "react-mdl/lib/Grid";
 
 class Education extends Component {
     render() {
-        return(
+        return (
             <Grid>
                 <Cell col={4}>
                     {this.props.startYear1} / {this.props.endYear1}
@@ -14,18 +14,18 @@ class Education extends Component {
                     <h5 style={{marginTop: "2px"}}>Front-end developer</h5>
                 </Cell>
                 <Cell col={4}>
-                    {this.props.startYear2} / {this.props.endYearFirst2}
-                </Cell>
-                <Cell col={8}>
-                    <h3 style={{marginBottom: "0px"}}>Minsk State College of Food Industry</h3>
-                    <p className={"skills-list"}>Minsk, Belarus</p>
-                    <h5 style={{marginTop: "2px"}}> Testing Web-oriented applications. QC engineer</h5>
-                </Cell>
-                <Cell col={4}>
                     {this.props.startYear3} / {this.props.endYear3}
                 </Cell>
                 <Cell col={8}>
                     <h3 style={{marginBottom: "0px"}}>Learning Technology Center</h3>
+                    <p className={"skills-list"}>Minsk, Belarus</p>
+                    <h5 style={{marginTop: "2px"}}>Testing Web-oriented applications. QC engineer</h5>
+                </Cell>
+                <Cell col={4}>
+                    {this.props.startYear2} / {this.props.endYear2}
+                </Cell>
+                <Cell col={8}>
+                    <h3 style={{marginBottom: "0px"}}>Minsk State College of Food Industry</h3>
                     <p className={"skills-list"}>Minsk, Belarus</p>
                     <h5 style={{marginTop: "2px"}}>Technician-technologist</h5>
                 </Cell>
